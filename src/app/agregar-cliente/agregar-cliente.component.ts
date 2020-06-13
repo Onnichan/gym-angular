c50046f6d059a3a43774005630a788dcf365fa75
Onnichan <dyangeltk17@gmail.com>
2020-06-12 20:56:08 -0500

    fourth commit

--- src/app/agregar-cliente/agregar-cliente.component.ts
+++ src/app/agregar-cliente/agregar-cliente.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AngularFireStorage} from '@angular/fire/storage';
 
 @Component({
   selector: 'app-agregar-cliente',
@@ -7,9 +9,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgregarClienteComponent implements OnInit {
 
-  constructor() { }
+  formularioCliente:FormGroup; 
+  porcentajeSubida:number = 0;
+
+  constructor(private fb:FormBuilder,private storage:AngularFireStorage) { }
 
   ngOnInit(): void {
+
+    this.formularioCliente = this.fb.group({
+      nombre:['',Validators.required],
+      apellido:['',Validators.required],
+      correo:['',Validators.compose([
+        Validators.required,Validators.email
+      ])],
+      cedula:['',Validators.maxLength(8)],
+      fechaNacimiento:['',Validators.required],
+      telefono:[''],
+      imgUrl:['',Validators.required]
+    })
+  }
+
+  agregar(){
+    console.log(this.formularioCliente.value)
+  }
+
+  subirImagen(evento){
+    // console.log(evento);
+    let archivo = evento.target.files[0];
+    let archivoName = new Date().getTime().toString();
+    let extension = archivo.name.toString().substr(archivo.name.toString().lastIndexOf('.'));
+    let ruta = 'clientes/'+archivoName+extension;
+    const referencia = this.storage.ref(ruta);
+    const tarea = referencia.put(archivo);
+    // console.log(archivo);
+    tarea.then((object)=>{
+      console.log('imagen subida');
+      referencia.getDownloadURL().subscribe((url)=>{
+        console.log(url);
+      });
+    })
+    tarea.percentageChanges().subscribe((percentaje)=>{
+      // console.log(percentaje);
+      this.porcentajeSubida = parseInt(percentaje.toString());
+    })
   }
 
 }
